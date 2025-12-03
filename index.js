0e48ce94ead8e5444a3b8cd1be5a367e57e2b577
Zia ul faker Khan <ziaulfakercc@gmail.com>
2025-12-03 14:46:58 +0500

    Switch to Node.js app - PDF version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 80;
-const version = process.env.VERSION || 'v1';
+const version = process.env.VERSION || 'v2';
 
 app.get('/', (req, res) => {
   res.send(`<h1>Hello from ${version}</h1><p>Full CI/CD Pipeline Working!</p>`);
