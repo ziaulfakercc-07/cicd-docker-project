7cc0a6943c92f42c24da9dbaba9614ba7a1e9360
Zia ul faker Khan <ziaulfakercc@gmail.com>
2025-12-03 14:50:46 +0500

    Deploy v2 – automatic deployment test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 80;
-const version = process.env.VERSION || 'v2';
+const version = process.env.VERSION || 'v2 – Fully Automated on My PC!';
 
 app.get('/', (req, res) => {
   res.send(`<h1>Hello from ${version}</h1><p>Full CI/CD Pipeline Working!</p>`);
